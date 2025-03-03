c0e18bea20a19b3e7c44f95ce5a54e44906b984c
Kennedy <145441761+Dravis-Xam@users.noreply.github.com>
2025-03-03 18:43:53 +0000

    modification to fit database

--- src/modules/items.js
+++ src/modules/items.js
@@ -16,6 +16,7 @@ const items = [
             image: "https://rukminim2.flixcart.com/image/850/1000/xif0q/screen-guard/screen-guard/m/q/9/po1-dva-zte-nubia-red-magic-9-pro-dvtech-original-imagwhg6rgegh9mg.jpeg?q=20&crop=false"
         },
         quantity: 200,
+        brand: "Red Magic",
     }, {
         name: "Infinix NOTE 40 I",
         price: 24000,
@@ -31,6 +32,7 @@ const items = [
             image: "https://api.priceinkenya.com/media/135685/conversions/Infinix-Note-40-Pro-a-original.webp"
         },
         quantity: 200,
+        brand: "infinite",
     }, {
         name: "Samsung S24",
         price: 96000,
@@ -46,6 +48,7 @@ const items = [
             image: "https://www.mobilehub.co.ke/storage/2024/10/Samsung-S24-Ultra-Price-in-Kenya-001-Mobilehub-Kenya.jpg"
         },
         quantity: 200,
+        brand: "samsung",
     }, {
         name: "Ipad",
         price: 42000,
@@ -61,6 +64,7 @@ const items = [
             image: "https://shop.ncsu.edu/Product%20Images/iPadAirM2_media-01.jpg?resizeid=2&resizeh=600&resizew=600"
         },
         quantity: 200,
+        brand: "ipad",
     }, {
         name: "OPPO Reno 7",
         price: 60000,
@@ -76,6 +80,7 @@ const items = [
             image: "https://pricezonekenya.com/wp-content/uploads/2023/01/Oppo-Reno-8-5G-Sak.jpg"
         },
         quantity: 200,
+        brand: "oppo",
     }
 ];
 
